e845b8bedbc3be8bc587c696c8f2fa11db8e150c
Jesse <jelloeater@gmail.com>
2016-11-10 00:08:58 -0500

    Updated methods Broke out new sections into logical units Need to flesh out JSON and GPIO pins

--- 8266relyctrl.py
+++ 8266relyctrl.py
@@ -8,26 +8,63 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 sock.listen(1) # don't queue up any requests
 
-# Loop forever, listening for requests:
-while True:
-    csock, caddr = sock.accept()
-    print ("Connection from: " + str(caddr))
-    req = csock.recv(1024) # get the request, 1kB max
-    get_req=str(req).split('GET /')[1].split('HTTP/1')[0]
-    print ('REQUEST RAW:\n')
+
+def parse_req(get_req):
+    print ('Get Req:')
     print(get_req)
-    print ('GET URL:\n')
-    print (req)
+    if 'favicon.ico' not in get_req:
+        get_req=get_req[1:]
+        data=get_req.split('=')
+        print(data)
+        pin_logic(data)
 
-    csock.sendall("""HTTP/1.0 200 OK
-    Content-Type: text/html
+def pin_logic(data):
+    if 'pin1' in data[0]:
+        if 'True' in data[1]:
+            print('yes')
+        else:
+            print('no')
 
-    <html>
-    <head>
-    </head>
-    <body>
-    {0}
-    </body>
-    </html>
-    """.format(req))
-    csock.close()
+def parse_logic(input):
+    input=input[1:]
+    data=input.split('=')
+    print(data)
+
+def run():
+    while True:
+        csock, caddr = sock.accept()
+        print ("\nConnection from: " + str(caddr))
+        req = csock.recv(1024) # get the request, 1kB max
+        get_req=str(req).split('GET /')[1].split('HTTP')[0]
+        print ('Req RAW:')
+        print (req)
+        output=parse_req(get_req)
+        csock.sendall("""HTTP/1.0 200 OK
+        Content-Type: text/html
+
+        <html>
+        <head>
+        </head>
+        <body>
+        <form action="" method="get">
+        <button name="pin1" value="True">P1-On</button>
+        </form>
+        <form action="" method="get">
+        <button name="pin1" value="False">P1-Off</button>
+        </form>
+        <br>
+        <form action="" method="get">
+        <button name="pin2" value="True">P2-On</button>
+        </form>
+        <form action="" method="get">
+        <button name="pin2" value="False">P2-Off</button>
+        </form>
+        <br>
+        OUTPUT:
+        {0}
+        </body>
+        </html>
+        """.format(str(output)))
+        csock.close()
+
+run()
